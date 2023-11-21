a5f885390feaf74eb234e47527e5ae1ce1f739c6
zimmu. <mzimmer@tktk.ee>
2023-11-21 20:17:16 +0200

    add Week15oopfile code

---
+++ Week15oopfiles/Program.cs
@@ -0,0 +1,53 @@
+﻿
+List<Movie> myMovies = new List<Movie>();
+string[] data = GetdataFomMyFile();
+//ReadDataFromArray(data);
+
+foreach(string line in data)
+{
+    string[] tempArray = line.Split(new char[] {';'}, StringSplitOptions.RemoveEmptyEntries);
+    Movie newMovie = new Movie(tempArray[0], tempArray[2]);
+    myMovies.Add(newMovie);
+}
+
+foreach(Movie movie in myMovies)
+{
+    Console.WriteLine($"One of my favourite movies {movie.Title} was released in {movie.Year}.");
+}
+
+
+static string[] GetdataFomMyFile()
+{
+    string filePath = @"C:\Users\Kasutaja\OneDrive\Dokumendid\data\movies.txt";
+    return File.ReadAllLines(filePath);
+}
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach(string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+
+class Movie
+{
+    string title; //fields
+    string year;
+
+    public string Title
+    {
+        get { return title; }
+    }
+
+    public string Year
+    {
+        get { return year; }
+    }
+    public Movie(string _title, string _year)
+    {
+        title = _title;
+        year = _year;
+    }
+}
